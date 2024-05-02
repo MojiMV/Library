1bce45620249121fb4f437f1bedffb23f224a095
Mojtaba Vahabnezhad <mojtabavahabnezhad@gmail.com>
2024-05-03 00:27:20 +0330

    create the basic ui

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readBefore){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readBefore = readBefore;
+}
+
+function addBookLibrary(){
+    
+}
